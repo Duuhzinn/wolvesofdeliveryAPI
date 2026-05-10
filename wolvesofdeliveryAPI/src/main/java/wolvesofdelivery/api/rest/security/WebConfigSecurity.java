f9d56eda69d5fc6d8656b22b913d890408e716c1
Eduardo Thomaz <duuhzinn23@gmail.com>
2026-05-10 12:19:17 -0300

    liberacao de cors

--- wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/security/WebConfigSecurity.java
+++ wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/security/WebConfigSecurity.java
@@ -43,7 +43,7 @@ public class WebConfigSecurity {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/", "/index",
-                                 "/wolvesofdeliveryAPI/login",
+                                 "/wolvesofdeliveryAPI/login", //AQUI
                                  "/login").permitAll()
                 .anyRequest().authenticated()
             )
