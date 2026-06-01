b7a02b9232912f1ca111db8a63a49adb8fb803aa
Eduardo Thomaz <duuhzinn23@gmail.com>
2026-06-01 11:02:27 -0300

    01/06/2026 - MELHORANDO A VELOCIDADE DA ENTREGA DE NOTIFICAÇÃO

--- wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/service/FirebaseNotificationService.java
+++ wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/service/FirebaseNotificationService.java
@@ -2,6 +2,10 @@ package wolvesofdelivery.api.rest.service;
 
 import org.springframework.stereotype.Service;
 
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.AndroidConfig.Priority;
+import com.google.firebase.messaging.ApnsConfig;
+import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 
@@ -16,6 +20,15 @@ public class FirebaseNotificationService {
 					.putData("body", mensagem)
 					.putData("corridaId", corridaId.toString())
 					.putData("despachanteId", cliente_id.toString())
+					.setAndroidConfig(AndroidConfig.builder()
+					.setPriority(Priority.HIGH)
+					.build())
+					.setApnsConfig(ApnsConfig.builder()
+							.setAps(Aps.builder()
+									.setContentAvailable(true)
+									.build())
+							.putHeader("apns-priority", "10")
+							.build())
 					.build();
 			FirebaseMessaging.getInstance().send(message);
 			return "Notificação Enviada";
