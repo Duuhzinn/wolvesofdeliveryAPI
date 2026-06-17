2273660ec6399f699a0d60e7bfd1b4354725194f
Eduardo Thomaz <duuhzinn23@gmail.com>
2026-06-17 11:34:55 -0300

    ATUALIZEI O CONTROLLER DO LOGRADOURO

--- wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/controller/LogradouroController.java
+++ wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/controller/LogradouroController.java
@@ -1,11 +1,21 @@
 package wolvesofdelivery.api.rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import wolvesofdelivery.api.rest.model.Logradouro;
 import wolvesofdelivery.api.rest.service.LogradouroService;
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/logradouros")
@@ -15,24 +25,24 @@ public class LogradouroController {
     @Autowired
     private LogradouroService logradouroService;
 
-    @GetMapping
+    @GetMapping(value = "/list", produces = "application/json")
     public ResponseEntity<List<Logradouro>> listar() {
-        return ResponseEntity.ok(logradouroService.listar());
+        return new ResponseEntity<List<Logradouro>>(logradouroService.listar(), HttpStatus.OK);
     }
 
-    @GetMapping("/buscar")
-    public ResponseEntity<List<Logradouro>> buscar(@RequestParam String rua) {
-        return ResponseEntity.ok(logradouroService.buscar(rua));
+    @GetMapping(value = "/search/{street}", produces = "application/json")
+    public ResponseEntity<List<Logradouro>> buscar(@PathVariable String rua) {
+        return new ResponseEntity<List<Logradouro>>(logradouroService.buscar(rua), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping(value = "/save", produces = "application/json")
     public ResponseEntity<Logradouro> salvar(@RequestBody Logradouro logradouro) {
-        return ResponseEntity.ok(logradouroService.salvar(logradouro));
+        return new ResponseEntity<Logradouro>(logradouroService.salvar(logradouro), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/delete/{id}", produces = "application/json")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         logradouroService.deletar(id);
-        return ResponseEntity.ok().build();
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 }
