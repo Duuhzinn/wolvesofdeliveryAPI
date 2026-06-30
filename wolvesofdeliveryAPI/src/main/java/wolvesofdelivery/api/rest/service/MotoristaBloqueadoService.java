840e82de0eae87a09e304b94603c769d4c7f9e01
Eduardo Thomaz <duuhzinn23@gmail.com>
2026-06-30 11:51:22 -0300

    CORRIGINDO ERROS DE SERVICE

--- wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/service/MotoristaBloqueadoService.java
+++ wolvesofdeliveryAPI/src/main/java/wolvesofdelivery/api/rest/service/MotoristaBloqueadoService.java
@@ -1,37 +1,37 @@
 package wolvesofdelivery.api.rest.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import wolvesofdelivery.api.rest.model.MotoristaBloqueado;
 import wolvesofdelivery.api.rest.repository.MotoristaBloqueadoRepository;
+
+import java.util.List;
 
 @Service
 public class MotoristaBloqueadoService {
 
-	@Autowired
+    @Autowired
     private MotoristaBloqueadoRepository motoristaBloqueadoRepository;
-	
-	public List<MotoristaBloqueado> listarPorRestaurante(Long restauranteId) {
+
+    public List<MotoristaBloqueado> listarPorRestaurante(Long restauranteId) {
         return motoristaBloqueadoRepository.findByRestauranteId(restauranteId);
     }
-	
-	public MotoristaBloqueado bloquear(Long restauranteId, Long motoristaId) {
+
+    public MotoristaBloqueado bloquear(Long restauranteId, Long motoristaId) {
         if (motoristaBloqueadoRepository.existsByRestauranteIdAndMotoristaId(restauranteId, motoristaId)) {
             return motoristaBloqueadoRepository.findByRestauranteIdAndMotoristaId(restauranteId, motoristaId).get();
         }
         MotoristaBloqueado bloqueado = new MotoristaBloqueado(restauranteId, motoristaId);
         return motoristaBloqueadoRepository.save(bloqueado);
     }
-	
-	public void desbloquear(Long restauranteId, Long motoristaId) {
+
+    @Transactional
+    public void desbloquear(Long restauranteId, Long motoristaId) {
         motoristaBloqueadoRepository.deleteByRestauranteIdAndMotoristaId(restauranteId, motoristaId);
     }
-	
-	public boolean isMotoristaBloqueado(Long restauranteId, Long motoristaId) {
+
+    public boolean isMotoristaBloqueado(Long restauranteId, Long motoristaId) {
         return motoristaBloqueadoRepository.existsByRestauranteIdAndMotoristaId(restauranteId, motoristaId);
     }
-	
 }
